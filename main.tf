abcc4ee815d68ef0d3a7bec6643552da062c8d62
Kishore Kalaiselvan <111948205+Kishore-Kalaiselvan@users.noreply.github.com>
2024-09-27 10:30:54 +0530

    Updated Key Value pair and Security id

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
-
 provider "aws" {
     region = "us-east-1"
   
 }
 
-resource "aws_instance" "Terraform_Project" {
-    ami = var.ami_value
-    instance_type = var.instance_type_value
+resource "aws_instance" "Terraform-Project" {
+    ami = var.ami_id
+    instance_type = var.aws_instance_type
+    key_name = "LearnDevops"
+    vpc_security_group_ids = ["sg-0a43221cca0a3cb2f"]
     tags = {
-      Name = "Terraform_Project"
+      Name = "Terraform-Project"
     }
-  
 }
